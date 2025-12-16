9060f1d5d45bf010cad54ab9cfa5373bc515058c
Abel <varugheseabel2005@gmail.com>
2025-12-17 02:36:34 +0530

    Experimented with alternative model iteration setting

--- train_model.py
+++ train_model.py
@@ -12,7 +12,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-model = LogisticRegression(max_iter=400)
+model = LogisticRegression(max_iter=300)
 model.fit(X_train, y_train)
 
 with open("model.pkl", "wb") as f:
